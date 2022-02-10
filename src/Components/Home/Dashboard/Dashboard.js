5d1c0a1bcc70667b32416005737c43d8f0495012
Kazal Ghosh <mrgkazal@gmail.com>
2022-02-10 20:36:39 +0600

    Dashboard design

--- src/Components/Home/Dashboard/Dashboard.js
+++ src/Components/Home/Dashboard/Dashboard.js
@@ -1,16 +1,10 @@
 import React from "react";
 import { Box } from "@mui/system";
 import Navbar from "../Navbar/Navbar";
-import { makeStyles } from "@mui/styles";
 import DashboardTab from "./DashboardTab";
 import DashboarBody from "./DashboarBody";
 
-const useStyles = makeStyles({
-  root: {},
-});
 const Dashboard = () => {
-  const classes = useStyles();
-
   return (
     <Box>
       <Navbar />
